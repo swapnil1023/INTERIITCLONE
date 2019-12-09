1a9834f4e2c83f3a73d0754a78b7e9cef02448c0
swapnil1023 <50006812+swapnil1023@users.noreply.github.com>
2019-12-09 14:31:18 +0530

    mapped logos to their string name

--- app/src/main/java/com/example/android/interiit/logoMap.java
+++ app/src/main/java/com/example/android/interiit/logoMap.java
@@ -12,6 +12,19 @@ public class logoMap {
     {
         logo.put("madras",R.drawable.madras);
         logo.put("indore",R.drawable.indore);
+        logo.put("kharagpur",R.drawable.Kharagpur);
+        logo.put("bhilai",R.drawable.IIT_Bhilai_logo);
+        logo.put("bombay",R.drawable.IIT_Bombay_color_logo);
+        logo.put("dhanbad",R.drawable.Dhanbad);
+        logo.put("delhi",R.drawable.Indian_Institute_of_Technology_Delhi_logo);
+        logo.put("roorkee",R.drawable.Indian_Institute_of_Technology_Roorkee_logo);
+        logo.put("ropar",R.drawable.Indian_Institute_of_Technology_Ropar_logo);
+        logo.put("tirupati",R.drawable.Indian_Institute_of_Technology_Tirupati_logo);
+        logo.put("kanpur",R.drawable.Kanpur);
+        logo.put("BHU",R.drawable.bhu);
+        logo.put("gandhinagar",R.drawable.gandhinagar);
+        logo.put("guwahati",R.drawable.guwahati);
+
     }
 
 }
